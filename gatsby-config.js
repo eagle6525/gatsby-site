79191e2d7329502b0d70eb86f7edd73778b072f6
Wayne Pichotta <wpichotta@truecar.com>
2020-11-13 16:17:53 -0800

    Add progressive web app information

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Pichotta`,
     description: `My personal site`,
-    siteUrl: 'https://inspiring-kowalevski-b8bd49.netlify.app/',
+    siteUrl: 'https://inspiring-kowalevski-b8bd49.netlify.app',
     author: `Wayne Pichotta`,
   },
   plugins: [
@@ -19,18 +19,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wayne Pichotta's Personal Website`,
+        short_name: `pichotta`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#524763`,
+        theme_color: `#524763`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
